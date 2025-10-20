93edd54f91778eff4602b4b2ee589009ad1b81fe
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-10-19 23:10:45 -0300

    chore: alterando caminho para pagina de login

--- components/header/index.js
+++ components/header/index.js
@@ -6,7 +6,7 @@ export function Header(){
         <div>
             <header className="header">
                 <Link href={'/'}><img src="../images/logo/logo_fundo-removebg.png" alt="Logo MyCondPets" className="logo" /></Link>
-                <Link className="login-btn" href={'/Teste'}>Login</Link>
+                <Link className="login-btn" href={'/login'}>Login</Link>
             </header>
         </div>
     )
