d50e83984def3300c41f6ef5b8343834073a45a3
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-10-22 22:55:56 -0300

    feat: criacao de busca de donos

--- app/_actions/users.js
+++ app/_actions/users.js
@@ -1,22 +1,18 @@
-import pool from '../../lib/db';
+import pool from '../../_lib/db';
 
 export default async function handler(req, res) {
-  // Permite apenas o método GET
   if (req.method !== 'GET') {
     res.setHeader('Allow', ['GET']);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 
-  let client; // Declara a variável do cliente fora do try para que seja acessível no finally
+  let client;
 
   try {
-    // 1. Pega uma conexão ("cliente") do pool
     client = await pool.connect();
-    
-    // 2. Executa a sua query SQL
-    const result = await client.query('SELECT id, name, email FROM users;');
-    
-    // 3. Retorna os resultados como JSON
+
+    const result = await client.query('SELECT don_cpf, don_nome, don_email, don_senha, don_contato FROM dono;');
+
     res.status(200).json(result.rows);
 
   } catch (error) {
@@ -24,7 +20,6 @@ export default async function handler(req, res) {
     res.status(500).json({ error: 'Internal Server Error' });
 
   } finally {
-    // 4. ESSENCIAL: Libera o cliente de volta para o pool
     if (client) {
       client.release();
     }
--- app/page.js
+++ app/page.js
@@ -1,6 +1,22 @@
 import "./styles.css";
+import pool from "@/app/_lib/db"
 
-export default function Home() {
+export default async function Home() {
+  
+  let donos = [];
+
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT don_cpf, don_nome, don_email, don_senha, don_contato FROM dono;');
+    donos = result.rows;
+    
+    client.release();
+
+
+  } catch (error) {
+    console.error("Erro ao buscar donos:", error);
+  }
+
   return (
     <main className="container">
       {/* Conteúdo principal */}
@@ -12,6 +28,17 @@ export default function Home() {
             <span className="highlight">patinha!</span>
           </p>
         </div>
+
+        {
+          donos.map((dono) => (
+            <div key={dono.don_cpf} className="dono-info">
+              <h2>Nome: {dono.don_nome}</h2>
+              <p>Email: {dono.don_email}</p>
+              <p>Senha: {dono.don_senha}</p>
+              <p>Contato: {dono.don_contato}</p>
+            </div>
+          ))
+        }
 
         <div className="image-container">
           <img src="../images/pet1.jpg" alt="Cachorro feliz" className="dog-image" />
