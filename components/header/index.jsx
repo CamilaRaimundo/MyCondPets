0eedfa13e6e4a053076507d3e9f586be83b0261d
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-11-20 17:45:31 -0300

    fix: correção no header

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,30 +1,49 @@
 "use client";
+
 import "./styles.css";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
+import { useRouter, usePathname } from "next/navigation";
+import { useEffect } from "react";
 
-export function Header(){
-      const { data: session } = useSession();
-    
-    return(
-        <div>
-            <header className="header">
-                <Link href={'/'}><img src="../images/logo/logo_fundo-removebg.png" alt="Logo MyCondPets" className="logo" /></Link>
-                <div className="navega">
-                    <Link href={'/telaInicialCond'} className="navega-item">Tela Inicial (Condomínio)</Link>
-                    <h3 className="navega-item">Notícias</h3>
-                    <Link href={'/perfilDono'} className="navega-item">Perfil</Link>
-                    <h3 className="navega-item">Pets</h3>
-                </div>
-                <Link className="login-btn" href={'/Teste'}>Login</Link>
-                {session ? (
-                    <nav>
-                        <button className="logout-btn" onClick={() => signOut()}>Sair</button>
-                    </nav>
-                ) :
-                    <Link className="login-btn" href={'/login'}>Login</Link>
-                }   
-            </header>
-        </div>
-    )
+export function Header() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  // useEffect(() => {
+  //   if (session?.user && !session.user.cpf && pathname !== "/perfilDono") {
+  //     router.push("/perfilDono");
+  //   }
+  // }, [session, pathname, router]);
+
+  if (status === "loading") {
+    return (
+      <div className="carregando">
+        <p>Carregando...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <header className="header">
+        <Link href={'/'}><img src="../images/logo/logo_fundo-removebg.png" alt="Logo MyCondPets" className="logo" /></Link>
+
+        {session ? (
+          <>
+            <nav className="menu">
+              <Link href={'/telaInicialCond'} className="navega-item">Tela Inicial (Condomínio)</Link>
+              <Link href={'/noticias'} className="navega-item">Notícias</Link>
+              <Link href={'/perfilDono'} className="navega-item">Perfil</Link>
+              <Link href={'/pets'} className="navega-item">Pets</Link>
+            </nav>
+            <button className="logout-btn" onClick={() => signOut()}>Sair</button>
+          </>
+        ) : (
+          <Link className="login-btn" href={'/login'}>Login</Link>
+        )}
+      </header>
+    </div>
+  );
 }
